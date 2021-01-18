409bca5b57eba3bbea5ce3c704f4f82b9d6fd827
ChrisYoung1048 <chrisyoung1048@gmail.com>
2021-01-17 17:06:35 -0800

    Finished part 1

--- Lab3_bitManipulation/header/simAVRHeader.h
+++ Lab3_bitManipulation/header/simAVRHeader.h
@@ -5,13 +5,19 @@
 #endif
 
 #include <avr/sleep.h>
-#include "../../../simavr/avr/avr_mcu_section.h"
+#include "../../../simavr-master/simavr/sim/avr/avr_mcu_section.h"
 AVR_MCU(F_CPU,"atmega1284");
 AVR_MCU_VCD_FILE("build/results/Lab3_bitManipulation_trace.vcd",1000);
 
 const struct avr_mmcu_vcd_trace_t _mytrace[] _MMCU_ = {
-    { AVR_MCU_VCD_SYMBOL("PINA0"), .mask = 1 << 0,.what = (void*)&PINA, } , // Example individual pin
-    { AVR_MCU_VCD_SYMBOL("PORTB"), .what = (void*)&PORTB, } , // Example full port
+	{ AVR_MCU_VCD_SYMBOL("PINA0"), .mask = 1 << 0,.what = (void*)&PINA, } , // Example individual pin
+	{ AVR_MCU_VCD_SYMBOL("PORTA"), .what = (void*)&PORTA, } , // Example full port
+	{ AVR_MCU_VCD_SYMBOL("PORTB"), .what = (void*)&PORTB, } ,
+	{ AVR_MCU_VCD_SYMBOL("PORTC"), .what = (void*)&PORTC, } ,
+	{ AVR_MCU_VCD_SYMBOL("PINA"), .what = (void*)&PINA, } ,
+	{ AVR_MCU_VCD_SYMBOL("PINB"), .what = (void*)&PINB, } ,
+	{ AVR_MCU_VCD_SYMBOL("PINC"), .what = (void*)&PINC, } ,
+	
 };
 
 /* Function to output through UART */
